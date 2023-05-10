743743caa98596816f77de660e17c1acbcd9e44b
kikuti <joaovictorkikuti@gmail.com>
2023-05-10 00:16:03 -0300

    feat: terminado requisito 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 from parsel import Selector
 import requests
 import time
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -47,4 +48,16 @@ def scrape_news(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    BASE_URL = "https://blog.betrybe.com/"
+    html_content = fetch(BASE_URL)
+    news_urls = scrape_updates(html_content)
+    all_news = []
+    for index in range(amount):
+        try:
+            all_news.append(scrape_news(fetch(news_urls[index])))
+        except IndexError:
+            html_content = fetch(scrape_next_page_link(html_content))
+            news_urls.extend(scrape_updates(html_content))
+            all_news.append(scrape_news(fetch(news_urls[index])))
+    create_news(all_news)
+    return all_news
