0f3cd7a8993c0957a72b73fec0c515f92c701f89
kikuti <joaovictorkikuti@gmail.com>
2023-05-10 03:16:11 -0300

    feat: terminado requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,11 @@
+from collections import Counter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    all_news = find_news()
+    all_categories = [new["category"] for new in all_news]
+    categories_counter = Counter(sorted(all_categories)).most_common(5)
+
+    return [category[0] for category in categories_counter]
